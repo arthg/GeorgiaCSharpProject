b9b04c9079b4cad8d8f18ee7fe78e171ceb7e1c7
Arthur Gorr <art.gorr@gmail.com>
2015-01-06 21:01:40 -0600

    Tortoise project

---
+++ ConsoleApplication1/ConsoleApplication1/01.SimpleSquare/SimpleSquare.cs
@@ -0,0 +1,21 @@
+﻿using SmallBasicFun;
+
+namespace SimpleSquare
+{
+	public class SimpleSquare
+	{
+		public static void Main()
+		{
+            Tortoise.Show();
+            Tortoise.SetSpeed(10);
+		    int sides =MessageBox.AskForInput("How many sides?");
+		    for (int i = 0; i < sides; i++)
+		    {
+                Tortoise.SetPenWidth((i+1)*2.5);
+		        Tortoise.SetPenColor(Colors.GetRandomColor());
+		        Tortoise.Move(i*3.5);
+		        Tortoise.Turn(-3*360.0/sides);
+		    }
+		}
+	}
+}
